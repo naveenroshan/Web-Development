db72c1ff3502756ca0dcdbd84fc3c0212cfeadd5
Naveen S <naveen.s2@happiestminds.com>
2021-03-10 11:20:40 +0530

    aut

--- Authentication-Security(app-brewery)/Secrets/app.js
+++ Authentication-Security(app-brewery)/Secrets/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.static("public"));
 
@@ -12,18 +13,66 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server is running")
 });
 
-app.get("/", function(req, res){
+//connecting to the  mongo local db
+const url = 'mongodb://localhost:27017/userDB';
+const mongodbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(url, mongodbOptions);
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function (req, res) {
     res.render("register");
 });
+
+//creating register page api
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+//creating login page api
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, function(err, founduser){
+        if(err){
+            console.log(err);
+        }else{
+            if(founduser){
+                if(founduser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
