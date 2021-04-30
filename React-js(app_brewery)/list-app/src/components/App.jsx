7fdc7f289d4a691c30df89f145e99ff5c95783f8
Naveen S <naveen.s2@happiestminds.com>
2021-04-30 13:35:13 +0530

    using map in react components

--- React-js(app_brewery)/list-app/src/components/App.jsx
+++ React-js(app_brewery)/list-app/src/components/App.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import Card from "./Card";
 import contactdetails from "../contacts";
 
-function App() {    
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-          name={contactdetails[0].name}
-          img={contactdetails[0].imgURL}
-          tel={contactdetails[0].phone}
-          email={contactdetails[0].email}
-      />
-      <Card
-          name={contactdetails[1].name}
-          img={contactdetails[1].imgURL}
-          tel={contactdetails[1].phone}
-          email={contactdetails[1].email}
-      />
-      <Card
-          name={contactdetails[2].name}
-          img={contactdetails[2].imgURL}
-          tel={contactdetails[2].phone}
-          email={contactdetails[2].email}
-      />
+      {contactdetails.map(createCard)}
     </div>
   );
 }
--- React-js(app_brewery)/list-app/src/components/Card.jsx
+++ React-js(app_brewery)/list-app/src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
 function Card(Props) {
-    return(
-<div className="card">
-        <div className="top">
-          <h2 className="name">{Props.name}</h2>
-          <img className="circle-img"
-            src={Props.img}
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">{Props.tel}</p>
-          <p className="info">{Props.email}</p>
-        </div>
-      </div>);
+  return (
+    <div className="card">
+      <div className="top">
+      <p className="name">{Props.id}</p>
+        <h2 className="name">{Props.name}</h2>
+        <img className="circle-img" src={Props.img} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{Props.tel}</p>
+        <p className="info">{Props.email}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
