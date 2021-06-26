1a5bc9274132142494d6be6541ff07b809044bd6
Naveen S <naveen.s2@happiestminds.com>
2021-06-26 11:06:59 +0530

    VueJS course day six

--- VueJs(NetNinja)/VueCLI/vuejs-project/src/App.vue
+++ VueJs(NetNinja)/VueCLI/vuejs-project/src/App.vue
@@ -5,14 +5,27 @@
       v-bind:title="title"
       v-on:changeTitle="updateTitle($event)"
     ></app-header>
+    <!--Slots for push HTML-->
+    <form-helper>
+      <!--naming the html with slots-->
+      <div slot="form-header">
+        <h3>This is the slot title form</h3>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
 
     <app-header v-bind:titleEventbus="titleEventbus"></app-header>
     <!--passing the props to the welcome components-->
     <!--since the props to be dynamic we are using v-bind -->
     <app-welcome v-bind:ninjas="ninjas"></app-welcome>
-    
+
     <app-footer v-bind:title="title"></app-footer>
-    
+
     <app-footer v-bind:titleEventbus="titleEventbus"></app-footer>
   </div>
 </template>
@@ -21,13 +34,15 @@
 import Welcome from "./components/Welcome.vue";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import Forms from "./components/Forms.vue"
 
 export default {
   //defining the component in the main app component
   components: {
     "app-header": Header,
     "app-footer": Footer,
-    "app-welcome": Welcome
+    "app-welcome": Welcome,
+    "app-forms": Forms
   },
   data() {
     return {
@@ -48,6 +63,26 @@ export default {
       this.title = updatedTitle;
     }
   }
+
+  //life cycle Hooks
+  // beforeCreate() {
+  //   alert("beforeCreate");
+  // },
+  // created() {
+  //   alert("created");
+  // },
+  // beforeMount() {
+  //   alert("beforeMount");
+  // },
+  // mounted() {
+  //   alert("mounted");
+  // },
+  // beforeUpdate() {
+  //   alert("beforeUpdated");
+  // },
+  // update() {
+  //   alert("update");
+  // }
 };
 </script>
 
